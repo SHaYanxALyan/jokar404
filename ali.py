87dcad827f63adc6d52bb74e9ce7d7b9da341c76
SHaYanxALyan <100460319+SHaYanxALyan@users.noreply.github.com>
2023-05-20 00:02:57 +0500

    Add files via upload

--- ali.py
+++ ali.py
@@ -439,7 +439,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/MR-ZIHAD/HTcrack/blob/main/approve.txt").text
+	r1=requests.get("https://https://raw.githubusercontent.com/SHaYanxALyan/jokar404/main/ak.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
